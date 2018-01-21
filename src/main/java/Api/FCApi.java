51e7f1eb50da8466c885579f854151ec166a9d3a
unknown <danish.ahmed92@live.com>
2018-01-21 21:03:04 +0100

    taskId as pathVariable, changed to keep client side mapping limited to <string,byte[]>

--- src/main/java/Api/FCApi.java
+++ src/main/java/Api/FCApi.java
@@ -3,10 +3,16 @@ package Api;
 public class FCApi {
     private final String taskId;
     private final byte[] data;
+    private double defactoScore;
 
     public FCApi(String taskId, byte[] data) {
         this.taskId = taskId;
         this.data = data;
+        this.defactoScore = 0;
+    }
+
+    public void setDefactoScore(double defactoScore) {
+        this.defactoScore = defactoScore;
     }
 
     public String getTaskId() {
@@ -15,5 +21,9 @@ public class FCApi {
 
     public byte[] getData() {
         return data;
+    }
+
+    public double getDefactoScore() {
+        return defactoScore;
     }
 }
--- src/main/java/Api/FCApiController.java
+++ src/main/java/Api/FCApiController.java
@@ -5,18 +5,15 @@ import java.util.Map;
 import FCWrapper.FactCheckBytes;
 import org.aksw.defacto.evidence.Evidence;
 import org.aksw.defacto.model.DefactoModel;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class FCApiController {
     private final double threshold = 0.7;
 
-    @RequestMapping(value="/execTask", method= RequestMethod.POST)
-    public String taskData(@RequestParam(value = "taskId", required = true) String taskId,
+    /*@RequestMapping(value="/execTask/{taskId}", method= RequestMethod.POST)
+    public String execT(@PathVariable(value = "taskId") String taskId,
                            @RequestParam(value = "data", required = true) byte[] data) {
         System.out.println(new String(data));
         Map<DefactoModel, Evidence> defactoModelEvidenceMap = FactCheckBytes.FactCheckFromBytes(taskId, data);
@@ -29,5 +26,44 @@ public class FCApiController {
         if (defactoScore >= threshold)
             return defactoScore + " true";
         return defactoScore + " false";
+    }*/
+
+    @RequestMapping(value="/execTask/{taskId}", method= RequestMethod.POST)
+    public FCApi execT(@PathVariable(value = "taskId") String taskId,
+                        @RequestParam(value = "data", required = true) byte[] data) {
+        System.out.println(new String(data));
+        Map<DefactoModel, Evidence> defactoModelEvidenceMap = FactCheckBytes.FactCheckFromBytes(taskId, data);
+
+        Map.Entry<DefactoModel, Evidence> entryIterator = defactoModelEvidenceMap.entrySet().iterator().next();
+        Evidence evidence = entryIterator.getValue();
+        double defactoScore = evidence.getDeFactoScore();
+
+        FCApi fcApi = new FCApi(taskId, data);
+        fcApi.setDefactoScore(defactoScore);
+
+        return fcApi;
+
+        // do threshold validation here
+//        if (defactoScore >= threshold)
+//            return defactoScore + " true";
+//        return defactoScore + " false";
     }
+
+    /*@RequestMapping(value="/task/{taskId}", method= RequestMethod.POST)
+    public FCApi task(@PathVariable("taskId") String taskId, @RequestBody FCApi FCApi) {
+        System.out.println(new String(FCApi.getData()));
+        Map<DefactoModel, Evidence> defactoModelEvidenceMap = FactCheckBytes.FactCheckFromBytes(taskId, FCApi.getData());
+
+        Map.Entry<DefactoModel, Evidence> entryIterator = defactoModelEvidenceMap.entrySet().iterator().next();
+        Evidence evidence = entryIterator.getValue();
+        double defactoScore = evidence.getDeFactoScore();
+
+        FCApi.setDefactoScore(defactoScore);
+
+        // do threshold validation here
+        if (defactoScore >= threshold)
+            return defactoScore + " true";
+        return defactoScore + " false";
+        return FCApi;
+    }*/
 }
