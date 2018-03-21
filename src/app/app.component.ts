a9e2b0b18d33fcd5db8179272857bc12ca466012
Fahad-Anwar <sjdff1@gmail.com>
2018-03-21 18:04:54 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,103 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  title = 'FactCheck';
+  btnText = 'Submit';
+  subject = '';
+  predicate = '';
+  object = '';
+  selection = '';
+  isFile = false;
+  file;
+  fileName = 'testName';
+  onClick() {
+    console.log('on click called');
+    //  if(this.subject ==''){
+    //    console.log("subject is null ");
+    //    alert("subject is null");
+    //   }else {
+    //     console.log("subject is not null ");
+    //    alert("subject is not null");
+    //   }
+    if (this.validateInput()) {
+      // send data
+    } else {
+      // return false;
+    }
+
+  }
+
+  uploadFile(e) {
+    console.log('file name before: ' + this.fileName);
+    console.log('file was: ' + this.file);
+    this.file = e.target.files[0];
+    this.fileName = this.file.name;
+    console.log('uploadFile is called file is: ' + this.file);
+    console.log('file name after: ' + this.fileName);
+  }
+
+
+  // Validates the user input
+  validateInput() {
+    console.log('selction is: ' + this.selection);
+    if (this.selection === 'file') {
+      console.log('file is ' + this.file);
+      if (this.file !== undefined && this.file != null && this.file !== '') {
+        if (this.file.name.endsWith('.ttl')) {
+          // Read file contents
+          const reader = new FileReader();
+          reader.onload = function () {
+            console.log('onLoad is called...');
+            const text = reader.result;
+            console.log(text);
+            // var output = document.getElementById('output');
+          };
+          reader.readAsText(this.file);
+
+          return true;
+        } else {
+          console.log('Input file is not valid, please select ttl File...! ');
+          alert('Input file is not valid, please select ttl File...! ');
+          return false;
+        }
+      } else {
+        alert('No file is selected, Please select ttl File...! ');
+        console.log('No file is selected, Please select ttl File...! ');
+      }
+
+
+    } else if (this.selection === 'text') {
+      console.log('text selection: ' + this.subject + '\n' + this.predicate + '\n' + this.object);
+      return true;
+    } else {
+      alert('Please select file or text before sending');
+    }
+  }
+  textSelection() {
+
+    document.getElementById('fileInput').removeAttribute('type');
+    document.getElementById('fileInput').setAttribute('type', 'file');
+
+    this.selection = 'text';
+    this.isFile = false;
+    this.file = '';
+    this.fileName = '';
+  }
+
+  fileSelection() {
+    this.isFile = true;
+    this.selection = 'file';
+    this.subject = '';
+    this.predicate = '';
+    this.object = '';
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
