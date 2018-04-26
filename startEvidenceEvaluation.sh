3ceb818b20887b68f8d13539f3138a052a46c413
CodeMetro <zafar.habeeb1989@gmail.com>
2018-04-26 17:04:56 +0200

    Removed unused files

--- startEvidenceEvaluation.sh
+++
@@ -1,3 +0,0 @@
-export MAVEN_OPTS="-Xmx12000m"
-mvn compile
-mvn exec:java -Dexec.mainClass="org.aksw.defacto.evaluation.DefactoEvaluation" -Dexec.args="train"
--- startTimePeriodEvaluation.sh
+++
@@ -1,3 +0,0 @@
-export MAVEN_OPTS="-Xmx30G"
-mvn -o compile
-mvn exec:java -Dexec.mainClass="org.aksw.defacto.evaluation.DefactoTimePeriodLearning" -Dexec.args="test"
