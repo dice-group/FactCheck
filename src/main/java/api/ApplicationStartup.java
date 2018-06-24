dafd7adb3a12456d3cc61be7c4be9992a2262bb4
oshando <oshando@campus.uni-paderborn.de>
2018-06-24 19:29:28 +0200

    Added logging and fixed HOBBIT request issue

--- src/main/java/api/ApplicationStartup.java
+++ src/main/java/api/ApplicationStartup.java
@@ -1,16 +1,12 @@
 package api;
+
 import org.aksw.defacto.Defacto;
-import org.aksw.defacto.DefactoDemo;
-import org.aksw.defacto.model.DefactoModel;
 import org.dice.factcheck.nlp.stanford.CoreNLPClient;
 import org.dice.factcheck.nlp.stanford.impl.CoreNLPLocalClient;
 import org.dice.factcheck.nlp.stanford.impl.CoreNLPServerClient;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class ApplicationStartup
@@ -25,15 +21,12 @@ public class ApplicationStartup
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
 
-            Defacto.init();
+        Defacto.init();
 
-            if (Defacto.DEFACTO_CONFIG.getBooleanSetting("corenlp", "USE_SERVER")) {
-                corenlpClient = new CoreNLPServerClient();
-            } else {
-                corenlpClient = new CoreNLPLocalClient();
-            }
-
-        return;
+        if (Defacto.DEFACTO_CONFIG.getBooleanSetting("corenlp", "USE_SERVER")) {
+            corenlpClient = new CoreNLPServerClient();
+        } else {
+            corenlpClient = new CoreNLPLocalClient();
+        }
     }
-
 }
--- src/main/java/api/Controller.java
+++ src/main/java/api/Controller.java
@@ -1,23 +1,24 @@
 package api;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Map;
-
 import org.aksw.defacto.Defacto;
 import org.aksw.defacto.evidence.Evidence;
 import org.aksw.defacto.model.DefactoModel;
-import rdf.TripleExtractor;
-import wrapper.FactCheckBytes;
-import wrapper.ModelTransform;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import rdf.TripleExtractor;
+import wrapper.ModelTransform;
 import wrapper.preprocessing.FCpreprocessor;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
+
+    private final Logger logger = LoggerFactory.getLogger(Controller.class);
 
     // To verify status of server
     @RequestMapping("/default")
@@ -29,30 +30,21 @@ public class Controller {
     @PostMapping("/execTask/")
     public FactcheckResponse execT(@RequestBody FactcheckResponse factcheckResponse) throws IOException {
 
-        System.out.println(factcheckResponse.getTaskid());
-        String taskId = factcheckResponse.getTaskid();
-        System.out.println(factcheckResponse.getFile());
+        logger.info("Task {} received with data: {}", factcheckResponse.getTaskid(), factcheckResponse.getFile());
 
+        String taskId = factcheckResponse.getTaskid();
         String fileData = factcheckResponse.getFile();
 
+        logger.info("Extracting data using TripleExtractor");
         TripleExtractor tripleExtractor = new TripleExtractor(fileData, false);
+
         FCpreprocessor fCpreprocessor = new FCpreprocessor(tripleExtractor.getSimplifiedData(), taskId, "");
-        DefactoModel defactoModel = new ModelTransform(fCpreprocessor, taskId).getDefactoModel();
+        Evidence evidence = getEvidence(fCpreprocessor, taskId);
 
-        defactoModel.corenlpClient = ApplicationStartup.corenlpClient;
-
-
-        Evidence evidence = Defacto.checkFact(defactoModel, Defacto.TIME_DISTRIBUTION_ONLY.NO);
-
-
-        /*// Conversion of data in string format to byte array
-        byte[] databyte = fileData.getBytes(StandardCharsets.UTF_8);
-        Map<DefactoModel, Evidence> defactoModelEvidenceMap = FactCheckBytes.FactCheckFromBytes(taskId, databyte);
-
-        Map.Entry<DefactoModel, Evidence> entryIterator = defactoModelEvidenceMap.entrySet().iterator().next();
-        Evidence evidence = entryIterator.getValue();*/
-
+        // Setting defacto score received from response
         double defactoScore = evidence.getDeFactoScore();
+        logger.info("Score {} returned for task {}", defactoScore, taskId);
+        factcheckResponse.setDefactoScore(defactoScore);
 
         //Setting proof sentences
         factcheckResponse.setComplexProofs(setProofSentences(evidence));
@@ -64,9 +56,6 @@ public class Controller {
         String[] p = predicate.split("/");
         factcheckResponse.predicate = p[p.length - 1];
 
-        // Setting defacto score received from response
-        factcheckResponse.setDefactoScore(defactoScore);
-
         return factcheckResponse;
     }
 
@@ -75,16 +64,24 @@ public class Controller {
                                          @RequestParam(value = "dataISWC", required = true) String dataISWC,
                                          @RequestParam(value = "fileTrace", required = true) String fileTrace) {
 
-        System.out.println("called by hobbit");
+        logger.info("Task {} received from HOBBIT", taskId);
 
         FCpreprocessor fCpreprocessor = new FCpreprocessor(dataISWC, taskId, fileTrace);
-        DefactoModel defactoModel = new ModelTransform(fCpreprocessor, taskId).getDefactoModel();
-        Evidence factEvidence = Defacto.checkFact(defactoModel, Defacto.TIME_DISTRIBUTION_ONLY.NO);
-        double defactoScore = factEvidence.getDeFactoScore();
+        Evidence factEvidence = getEvidence(fCpreprocessor, taskId);
 
-        return new FactCheckHobbitResponse(taskId, defactoScore, fCpreprocessor.getFileTrace());
+        logger.info("Score {} returned for task {}", factEvidence.getDeFactoScore(), taskId);
+
+        return new FactCheckHobbitResponse(taskId, factEvidence.getDeFactoScore(), fCpreprocessor.getFileTrace());
     }
 
+    //Return evidence object for task specified
+    private Evidence getEvidence(FCpreprocessor preprocessor, String taskId) {
+
+        DefactoModel defactoModel = new ModelTransform(preprocessor, taskId).getDefactoModel();
+        defactoModel.corenlpClient = ApplicationStartup.corenlpClient;
+
+        return Defacto.checkFact(defactoModel, Defacto.TIME_DISTRIBUTION_ONLY.NO);
+    }
 
     private ArrayList<ComplexProofs> setProofSentences(Evidence evidence) {
         ArrayList<ComplexProofs> complexProofs = new ArrayList<>();
@@ -93,32 +90,4 @@ public class Controller {
         });
         return complexProofs;
     }
-/*
-    public static void main(String[] args) throws IOException {
-        String fileData = "@prefix fbase: <http://rdf.freebase.com/ns> .\n" +
-                "@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> .\n" +
-                "@prefix dbo:   <http://dbpedia.org/ontology/> .\n" +
-                "@prefix owl:   <http://www.w3.org/2002/07/owl#> .\n" +
-                "@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> .\n" +
-                "@prefix skos:  <http://www.w3.org/2004/02/skos/core#> .\n" +
-                "\n" +
-                "<http://dbpedia.org/resource/Albert_Einstein>\n" +
-                "\tdbo:award\t<http://dbpedia.org/resource/Nobel_Prize_in_Physics> .\n" +
-                "\n" +
-                "<http://dbpedia.org/resource/Albert_Einstein>\t\n" +
-                "\trdfs:label\t\"Albert Einstein\" .\n" +
-                "\n" +
-                "<http://dbpedia.org/resource/Nobel_Prize_in_Physics>\n" +
-                "\trdfs:label\t\"Nobel Prize in Physics\" .";
-        String taskId = "task1";
-
-        TripleExtractor tripleExtractor = new TripleExtractor(fileData, false);
-        FCpreprocessor fCpreprocessor = new FCpreprocessor(tripleExtractor.getSimplifiedData(), taskId, "");
-        DefactoModel defactoModel = new ModelTransform(fCpreprocessor, taskId).getDefactoModel();
-        Evidence evidence = Defacto.checkFact(defactoModel, Defacto.TIME_DISTRIBUTION_ONLY.NO);
-
-        double defactoScore = evidence.getDeFactoScore();
-        System.out.println(defactoScore);
-    }
- */
 }
