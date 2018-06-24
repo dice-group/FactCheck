22eecf6a3ae86725d2b747cda239dd07352fc428
Fahad-Anwar <sjdff1@gmail.com>
2018-06-24 22:01:55 +0200

    implemented: pages (about, FAQ, PageNotFound). added: nav menu, footer, UIupdated.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ListService } from './list.service';
 import {MatTabsModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatExpansionModule} from '@angular/material/expansion';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // import {LocalStorageModule} from 'angular-local-storage.min.js';
 import { NgxSpinnerModule } from 'ngx-spinner';
@@ -14,11 +15,20 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatDialogModule} from '@angular/material';
 import { DialogComponent } from './dialog/dialog.component';
 import { StatuscodesService } from './statuscodes.service';
+import { AboutComponent } from './about/about.component';
+import { FaqComponent } from './faq/faq.component';
+import { routes } from './app.router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DialogComponent,
+    AboutComponent,
+    FaqComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
@@ -29,7 +39,9 @@ import { StatuscodesService } from './statuscodes.service';
     NgxSpinnerModule,
     MatTooltipModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatExpansionModule,
+    routes
     // LocalStorageModule
   ],
   entryComponents: [DialogComponent],
