774442ac3a13d29308e3cff83bf379cd4cd2987d
CodeMetro <zafar.habeeb1989@gmail.com>
2018-08-15 18:50:15 +0200

    Adjusted repository to create factcheck-core

---
+++ factcheck-core/src/main/java/org/aksw/defacto/ml/feature/fact/impl/WordnetExpensionFeature.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package org.aksw.defacto.ml.feature.fact.impl;
+
+import java.io.File;
+import java.util.List;
+
+import org.aksw.defacto.Defacto;
+import org.aksw.defacto.boa.BoaPatternSearcher;
+import org.aksw.defacto.boa.Pattern;
+import org.aksw.defacto.evidence.ComplexProof;
+import org.aksw.defacto.evidence.Evidence;
+import org.aksw.defacto.ml.feature.fact.AbstractFactFeatures;
+import org.aksw.defacto.ml.feature.fact.FactFeature;
+import org.aksw.defacto.wordnet.WordNetExpansion;
+
+
+/**
+ * @author Daniel Gerber <dgerber@informatik.uni-leipzig.de>
+ *
+ */
+public class WordnetExpensionFeature implements FactFeature {
+	
+	private static WordNetExpansion wordnetExpansion;
+
+//    WordNetExpansion wordnetExpansion = new WordNetExpansion(new File(WordnetExpensionFeature.class.getResource("/wordnet/dict").getFile()).getAbsolutePath());
+	public static void init()
+	{
+		wordnetExpansion = new WordNetExpansion(Defacto.DEFACTO_CONFIG.getStringSetting("evidence", "WORDNET_DICTIONARY"));
+	}
+    
+    
+    BoaPatternSearcher searcher = new BoaPatternSearcher();
+    
+    /* (non-Javadoc)
+     * @see org.aksw.defacto.ml.feature.fact.FactFeature#extractFeature(org.aksw.defacto.evidence.ComplexProof)
+     */
+    @Override
+    public void extractFeature(ComplexProof proof, Evidence evidence) {
+        
+        double similarity = 0;
+
+        List<Pattern> patterns = searcher.querySolrIndex(evidence.getModel().getPropertyUri(), 20, 0, proof.getLanguage());
+        
+        for ( Pattern pattern : patterns ) {
+        	similarity = Math.max(similarity, wordnetExpansion.getExpandedJaccardSimilarity(proof.getProofPhrase(), pattern.getNormalized()));
+        }
+        
+        if ( Double.isInfinite(similarity) || Double.isNaN(similarity) ) proof.getFeatures().setValue(AbstractFactFeatures.WORDNET_EXPANSION, 0D);
+        else proof.getFeatures().setValue(AbstractFactFeatures.WORDNET_EXPANSION, similarity);
+    }
+    
+    public static void main(String[] args) {
+
+    	System.out.println(new File(WordnetExpensionFeature.class.getResource("/wordnet/dict").getFile()).getAbsolutePath());
+        System.out.println(new WordNetExpansion(new File(WordnetExpensionFeature.class.getResource("/wordnet/dict").getFile()).getAbsolutePath()).getExpandedJaccardSimilarity(", the director of", "the director of"));
+    }
+}
